b8f50d229dc1a6f88538f0a7d6092aefe60b8544
seitamuro <seitamuro0210@gmail.com>
2023-08-05 22:40:55 +0900

    画像を切り出すアルゴリズム完成

--- sketch.ts
+++ sketch.ts
@@ -11,10 +11,10 @@ const sketch = (p: p5) => {
     let genomes: Genome[] = [];
 
     p.setup = () => {
-        p.background(255);
+        p.background(0);
         image_width = img.width;
         image_height = img.height;
-        p.createCanvas(image_width * population, image_height * 2);
+        p.createCanvas(image_width * population, image_height * 2, p.P2D);
         for (let i = 0; i < population; i++) {
             genomes.push(new Genome());
         }
@@ -32,6 +32,8 @@ const sketch = (p: p5) => {
 
         // 正解画像を表示
         p.image(img, 0, 0);
+        p.fill(0, 0, 255);
+        p.rect(image_width * 2, 0, image_width * population, image_height);
 
         // GAを実行
         const new_genomes: Genome[] = [];
@@ -48,6 +50,7 @@ const sketch = (p: p5) => {
         }
 
         // 画面上に個体の画像を表示
+        genomes[0].show(image_width, 0);
         new_genomes.forEach((genome, i) => {
             genome.show(image_width * i, image_height);
         })
@@ -57,6 +60,26 @@ const sketch = (p: p5) => {
         for (let i = 0; i < population; i++) {
             new_genomes[i].fitness = fitness(img.pixels, get_image_pixels(p.pixels, i));
         }
+
+        const img1 = get_image_pixels(p.pixels, 0);
+        console.log("img1.length: ", img1.length);
+        console.log("image_width, image_height: ", image_width, image_height);
+        for (let i = 0; i < image_width; i++) {
+            for (let j = 0; j < image_height; j++) {
+                const pixelIndex = (j * image_width * population + i) * 4;
+                //p.pixels[pixelIndex + 0] = p.pixels[(i * image_width * 4 + j) * 4 + 0]; // Red
+                //p.pixels[pixelIndex + 1] = p.pixels[(i * image_width * 4 + j) * 4 + 1]; // Red
+                //p.pixels[pixelIndex + 2] = p.pixels[(i * image_width * 4 + j) * 4 + 2]; // Red
+                //p.pixels[pixelIndex + 3] = p.pixels[(i * image_width * 4 + j) * 4 + 3]; // Red
+                //console.log("rgba: ", img1[i * image_width + j + 0], img1[i * image_width + j + 1], img1[i * image_width + j + 2], img1[i * image_width + j + 3])
+                p.pixels[pixelIndex + 0] = img1[(j * image_width + i) * 4 + 0]; // Red
+                p.pixels[pixelIndex + 1] = img1[(j * image_width + i) * 4 + 1]; // Green
+                p.pixels[pixelIndex + 2] = img1[(j * image_width + i) * 4 + 2]; // Blue
+                p.pixels[pixelIndex + 3] = img1[(j * image_width + i) * 4 + 3]; // Alpha
+            }
+        }
+        p.updatePixels();
+
 
         // 次世代を選択
         genomes = genomes.concat(new_genomes);
@@ -71,18 +94,27 @@ const sketch = (p: p5) => {
 
     const get_image_pixels = (img: number[], idx: number): number[] => {
         const _img: number[] = [];
-        const next_pixels = image_width * population * 4;
-        const base = next_pixels * image_height;
+        const offset = (image_width * population * image_height) * 4 + image_width * idx * 4; // 正解画像 + idx画像
         const indexes = [];
-        for (let i = 0; i < image_height; i++) {
-            const offset = base + idx * image_width * 4;
-            for (let j = offset + next_pixels * i; j < offset + next_pixels * i + image_width * 4; j++) {
-                indexes.push(j);
+        for (let j = 0; j < image_height; j++) {
+            for (let i = 0; i < image_width; i++) {
+                const index = offset + (j * image_width * population + i) * 4;
+                indexes.push(index + 0);
+                indexes.push(index + 1);
+                indexes.push(index + 2);
+                indexes.push(index + 3);
             }
         }
         for (let i = 0; i < indexes.length; i++) {
+            /*if (i % 4 === 0 || i % 4 == 1 || i % 4 == 2) {
+                _img.push(img[image_width * 4 * 4]);
+            } else {
+                _img.push(255);
+            }*/
             _img.push(img[indexes[i]]);
         }
+        console.log("img: ", img);
+        console.log("_img: ", _img);
         return _img;
     }
 
@@ -180,11 +212,43 @@ const sketch = (p: p5) => {
 
     const fitness = (img1: number[], img2: number[]): number => {
         let sum = 0;
-        for (let i = 0; i < img1.length; i++) {
-            sum += Math.abs(img1[i] - img2[i]);
+        for (let i = 0; i < img1.length; i += 4) {
+            const [h1, s1, l1] = rgbToHsl(img1[i], img1[i + 1], img1[i + 2]);
+            const [h2, s2, l2] = rgbToHsl(img2[i], img2[i + 1], img2[i + 2]);
+            sum += Math.abs(h1 - h2) ^ 2;
         }
         return sum;
     }
+}
+
+function rgbToHsl(r: number, g: number, b: number): [number, number, number] {
+    r /= 255.0;
+    g /= 255.0;
+    b /= 255.0;
+
+    const maxVal = Math.max(r, g, b);
+    const minVal = Math.min(r, g, b);
+
+    let h = (maxVal + minVal) / 2;
+    let s = (maxVal + minVal) / 2;
+    const l = (maxVal + minVal) / 2;
+
+    if (maxVal === minVal) {
+        h = s = 0; // achromatic
+    } else {
+        const d = maxVal - minVal;
+        s = l > 0.5 ? d / (2.0 - maxVal - minVal) : d / (maxVal + minVal);
+
+        switch (maxVal) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+
+        h /= 6;
+    }
+
+    return [h * 360, s * 100, l * 100];
 }
 
 new p5(sketch)
